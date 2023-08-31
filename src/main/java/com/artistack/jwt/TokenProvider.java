ba5b22a3721d306dfb7486e743ea570e6acb3d60
Mason Kim <mskim9967@gmail.com>
2023-08-31 14:46:37 +0900

    fix: access token 만료기한 변경

--- src/main/java/com/artistack/jwt/TokenProvider.java
+++ src/main/java/com/artistack/jwt/TokenProvider.java
@@ -28,7 +28,7 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 3;            // 3분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24L;            // 3분
     private static final long ADMIN_ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 30L;  // 30일
 
